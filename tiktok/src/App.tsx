af02f66c5e8d6c7cd9f1a10a7c4f68212b11dc0a
goose956 <132108181+goose956@users.noreply.github.com>
2025-06-10 18:37:27 +0100

    Add tiktok/src/App.tsx

---
+++ tiktok/src/App.tsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
+import AdminPanel from './pages/AdminPanel';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/admin" element={<AdminPanel />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
